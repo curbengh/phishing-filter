e1b051b2fc4755538516988626e60d85cb513470
MDLeom <2809763-curben@users.noreply.gitlab.com>
2025-03-08 00:07:04 +0000

    fix: remove response header
    showing it will stop showing download progress/size

--- src/script.sh
+++ src/script.sh
@@ -12,7 +12,7 @@ if [ -n "$BASH_VERSION" ]; then
   shopt -s expand_aliases
 fi
 
-alias curl="curl -L -D -"
+alias curl="curl -L"
 alias rm="rm -rf"
 
 ## Use GNU grep, busybox grep is not as performant
